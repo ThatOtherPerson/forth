9137217bdb8e6decde0ab4fb0a8c2eeb4a947dca
David Beckley <beckl.ds@gmail.com>
2017-08-16 17:03:24 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::io::{self, Write};
+
+fn main() {
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).unwrap();
+
+        println!("< {}", buffer);
+    }
+}
